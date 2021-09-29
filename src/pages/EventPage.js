ccc7442c9ab08e98539b6cea7d16f1e8bcdb5ba8
irina <irina.konovalova455@gmail.com>
2021-09-29 21:56:53 +0300

    events page layout and data

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import "./event.css";
+import { Col, Container, Row, Image } from "react-bootstrap";
 import { useParams } from "react-router";
 import Video from "../components/Video/Video";
 import WaitingForStream from "../components/WaitingForStream/WaitingForStream";
@@ -16,53 +17,68 @@ const EventPage = () => {
   }
 
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col>
-            <h1 className="display-4">{eventResult.title}</h1>
-          </Col>
-          <Col>????????</Col>
-        </Row>
-        <Row>
-          <Col lg={9}>
-            <WaitingForStream
-              startDate={eventResult.startDate}
-              startTime={eventResult.startTime}
-            >
-              {(streamHasStarted) => (
-                <>
-                  {streamHasStarted && (
-                    <Video
-                      url={eventResult.streamUrl}
-                      type={eventResult.streamVideoType}
-                    />
-                  )}
-                  {!streamHasStarted && (
-                    <>
-                    <h2>Striimi alkaa joskus myöhemmin, tässä promo/arkisto video (placeholder atm)</h2>
-                    <Video
-                    url={eventResult.archiveVideoUrl}
-                    type={eventResult.archiveVideoType}
+    <Container fluid="md">
+      <Row>
+        <Col>
+          <h1 className="display-4">{eventResult.title}</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>
+            Striimi alkaa {eventResult.startDate} klo. {eventResult.startTime}{" "}
+          </h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <WaitingForStream
+            startDate={eventResult.startDate}
+            startTime={eventResult.startTime}
+          >
+            {(streamHasStarted) => (
+              <>
+                {streamHasStarted && (
+                  <Video
+                    url={eventResult.streamUrl}
+                    type={eventResult.streamVideoType}
                   />
-                  </>       
-                  )}
-                </>
-              )}
-            
-            </WaitingForStream>
-          </Col>
-          <Col>
-            Alkaa päivänä: {eventResult.startDate}
-            <br />
-            Alkaa klo: {eventResult.startTime}
-            <br />
-          </Col>
-        </Row>
-      </Container>
-      <br />
-      {/*  */}
-    </div>
+                )}
+                {!streamHasStarted && (
+                  <>
+                    <h2>
+                      Striimi alkaa joskus myöhemmin, tässä promo/arkisto video
+                      (placeholder atm)
+                    </h2>
+                    <Video
+                      url={eventResult.archiveVideoUrl}
+                      type={eventResult.archiveVideoType}
+                    />
+                  </>
+                )}
+              </>
+            )}
+          </WaitingForStream>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        <Col>
+          <Image
+            src={eventResult.speaker.image}
+            alt="speaker"
+            roundedCircle
+            width="100"
+            height="100"
+            className="speaker-img"
+          />
+          <p>{eventResult.speaker.name}</p>
+          <p>{eventResult.speaker.company}</p>
+        </Col>
+        <Col lg={9} className="d-flex align-items-center">
+          <p>{eventResult.description}</p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
