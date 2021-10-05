47e54788ccb6acf4ef111f50ca02ba5468c05e47
AiliAi <rebane@gmail.com>
2021-10-05 23:15:28 +0300

    styled countdown

--- src/components/CountDown/CountDown.js
+++ src/components/CountDown/CountDown.js
@@ -1,53 +1,64 @@
 import React from "react";
+import "./countdown.scss";
 
 const calculateTimeLeft = () => {
-  const now = new Date().getTime();
-  const countDownDate = new Date("Mar 23, 2022 12:00:00").getTime();
-  const difference = countDownDate - now;;
-  let timeLeft = {};
+    const now = new Date().getTime();
+    const countDownDate = new Date("Oct 6, 2021 23:13:00").getTime();
+    const difference = countDownDate - now;
 
-  if (difference > 0) {
-    timeLeft = {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((difference / 1000 / 60) % 60),
-      seconds: Math.floor((difference / 1000) % 60)
-    };
-  }
+    let timeLeft = {};
 
-  return timeLeft;
+    if (difference > 0) {
+        timeLeft = {
+            "päivää": Math.floor(difference / (1000 * 60 * 60 * 24)),
+            "tuntia": Math.floor((difference / (1000 * 60 * 60)) % 24),
+            "minuuttia": Math.floor((difference / 1000 / 60) % 60),
+            "sekuntia": Math.floor((difference / 1000) % 60)
+        };
+    }
+
+    return timeLeft;
 }
 
 const CountDown = () => {
     const [timeLeft, setTimeLeft] = React.useState(calculateTimeLeft());
-  
+
     React.useEffect(() => {
-      const id = setTimeout(() => {
-        setTimeLeft(calculateTimeLeft());
-      }, 1000);
-  
-      return () => {
-        clearTimeout(id);
-      };
+        const id = setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => {
+            clearTimeout(id);
+        };
     });
-  
+
+
     const timerComponents = Object.keys(timeLeft).map(interval => {
-      if (!timeLeft[interval]) {
-        return;
-      }
-  
-      return (
-        <span>
-          {timeLeft[interval]} {interval}{" "}
-        </span>
-      );
+        if (!timeLeft[interval]) {
+            if (interval === "päivää" || interval === "tuntia" || interval === "minuuttia") {
+                return "";
+            }
+        }
+
+        return (
+            <div className="space">
+                <div className="font-wight">
+                    {timeLeft[interval]}
+                </div>
+                <div>
+                    {interval}
+                </div>
+            </div>
+        );
     });
-  
+
+
     return (
-      <div>
-        {timerComponents.length ? timerComponents : <span>Time's up!</span>}
-      </div>
+        <div className="d-flex flex-row" md="auto">
+            {timerComponents.length ? timerComponents : ""}
+        </div>
     );
-  }
+}
 
 export default CountDown;
