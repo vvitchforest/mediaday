ae9ac133b158d292b02683ae4b0f70b1f3d35e73
irina <irina.konovalova455@gmail.com>
2021-10-05 23:26:21 +0300

    navbar and events drawer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
 
-import React from 'react';
+import React, { useState } from "react";
 
-import { Switch, Route, Link } from "react-router-dom";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Switch, Route, Link, NavLink } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import "./styles.scss";
 
 import AboutPage from "./pages/AboutPage";
 import PromoVideo from "./pages/PromoVideo";
@@ -13,13 +14,13 @@ import CountDown from "./components/CountDown/CountDown";
 import logo from "./logo.svg";
 import bgPicture from "./images/bg1.jpg";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
-import eventData from "./data/events.json"
-import { Col, Container, Row, Image, Button} from "react-bootstrap";
-import InfoCard from './components/InfoCard/InfoCard';
-import { AiFillSmile } from 'react-icons/ai';
-import { BiMoviePlay } from 'react-icons/bi';
-import { ImBubbles3 } from 'react-icons/im';
-import { BiLink } from 'react-icons/bi';
+import eventData from "./data/events.json";
+import { Col, Container, Row, Image, Button, Offcanvas } from "react-bootstrap";
+import InfoCard from "./components/InfoCard/InfoCard";
+import { AiFillSmile } from "react-icons/ai";
+import { BiMoviePlay } from "react-icons/bi";
+import { ImBubbles3 } from "react-icons/im";
+import { BiLink } from "react-icons/bi";
 
 const Completionist = () => <span>You are good to go!</span>;
 
@@ -30,15 +31,28 @@ const renderer = ({ hours, minutes, seconds, completed }) => {
     return <Completionist />;
   } else {
     // Render a countdown
-    return <span>{hours}:{minutes}:{seconds}</span>;
+    return (
+      <span>
+        {hours}:{minutes}:{seconds}
+      </span>
+    );
   }
 };
 
-
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="App">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="px-5 border-bottom"
+        fixed="top"
+      >
         <Navbar.Brand as={Link} to="/">
           <img
             src={logo}
@@ -52,42 +66,53 @@ function App() {
         <NavbarCollapse>
           <Nav defaultActiveKey="/home" className="me-auto">
             <Nav.Item>
-              <Nav.Link as={Link} to="/">
+              <Nav.Link exact activeClassName="active" as={NavLink} to="/">
                 Home
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link as={Link} to="/about">
+              <Nav.Link exact activeClassName="active" as={NavLink} to="/about">
                 About
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link as={Link} to="/schedule">
+              <Nav.Link
+                exact
+                activeClassName="active"
+                as={NavLink}
+                to="/schedule"
+              >
                 Schedule
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link as={Link} to="/promovideo">
-                Tapahtuman promo
-              </Nav.Link>
+              <Nav.Link onClick={handleShow}>Events</Nav.Link>
             </Nav.Item>
-            <NavDropdown title="Events">
-              <NavDropdown.Item as={Link} to="/event/mediaday-01">
-                Tulevaisuuden ratkaisut elokuvatuotannossa
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/event/mediaday-02">
-              Käyttäjäkokemuksen merkitys IT-järjestelmissä
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/event/mediaday-03">
-              Yrittäjyys korkean teknologian alalla
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/event/mediaday-04">
-              Kamerauutuudet indie-tuotannossa
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/event/mediaday-05">
-              Minne menet, suomalainen AV-ala?
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Offcanvas
+              show={show}
+              onHide={handleClose}
+              scroll="true"
+              keyboard="true"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title className="mx-5 mt-2">MediaDay events</Offcanvas.Title>
+              </Offcanvas.Header>
+
+              <Offcanvas.Body className="fs-5">
+                {eventData.events.map(({ videoUrl, title }) => (
+                  <Nav.Item key={title}>
+                    <Nav.Link
+                      as={NavLink}
+                      to={`/event/${videoUrl}`}
+                      className="event-drawer mb-3"
+                      activeClassName="event-drawer-active"
+                    >
+                      {title}
+                    </Nav.Link>
+                  </Nav.Item>
+                ))}
+              </Offcanvas.Body>
+            </Offcanvas>
           </Nav>
         </NavbarCollapse>
       </Navbar>
@@ -101,75 +126,72 @@ function App() {
         <Route path="/schedule">
           <SchedulePage />
         </Route>
-        <Route path="/promovideo">
-          <PromoVideo />
-        </Route>
         <Route path="/event/:id" children={<EventPage />} />
         <Route path="/">
           <header>
-            <Image src={bgPicture} className="img"/>
+            <Image src={bgPicture} className="img" />
             <Container className="text-over-img">
-            <Row xs="auto">
-              <h1 className="display-2 mediaday">MEDIADAY</h1>
-            </Row>
-            <Row xs="auto">
-              <p className="slogan-txt">LET'S GET INSPIRED!</p>
-            </Row>
-            <Row xs="auto">
-              <p className="header-txt">Metropolia ammattikorkeakoulu</p>
-            </Row>
-            <Row xs="auto">
-              <p className="header-txt">23.3.2022 Karaportti 2, Espoo</p>
-            </Row>
-            <Row className="justify-content-md-center extra-margin-top">
-              <Col md="auto">
-                <Button variant="purple">Striimi</Button>
-              </Col>
-            </Row>
-            <Row className="justify-content-md-center extra-margin-bottom mt-4"> 
-              <Col md="auto">
-                <CountDown />    
-              </Col>    
-            </Row>
+              <Row xs="auto">
+                <h1 className="display-2 mediaday">MEDIADAY</h1>
+              </Row>
+              <Row xs="auto">
+                <p className="slogan-txt">LET'S GET INSPIRED!</p>
+              </Row>
+              <Row xs="auto">
+                <p className="header-txt">Metropolia ammattikorkeakoulu</p>
+              </Row>
+              <Row xs="auto">
+                <p className="header-txt">23.3.2022 Karaportti 2, Espoo</p>
+              </Row>
+              <Row className="justify-content-md-center extra-margin-top">
+                <Col md="auto">
+                  <Button variant="purple">Striimi</Button>
+                </Col>
+              </Row>
+              <Row className="justify-content-md-center extra-margin-bottom mt-4">
+                <Col md="auto">
+                  <CountDown />
+                </Col>
+              </Row>
             </Container>
           </header>
           <body>
-          <Container>
-            <Row xs={1} md={2} className="g-4">
-              <Col>
-                <InfoCard 
-                number="01"
-                title="MediaDay 2022"
-                text="Metropolia ammattikorkeakoulu järjestää Mediaday-tapahtuman pitkän tauon jälkeen keväällä 2022."
-                icon={<BiMoviePlay />}
-                />
-              </Col>
-              <Col>
-                <InfoCard 
-                number="02"
-                title="Puhujia eri teollisuudenaloilta"
-                text="Huipputason ammattilaisten puheenvuoroja, mukana onta rautaista puhujaa monelta mediakentän osa-alueelta."
-                icon={<ImBubbles3 />}
-                />
-              </Col>
-              <Col>
-                <InfoCard 
-                number="03"
-                title="Mediaday yhdistää"
-                text="Mediaday tuo opiskelijat ja yritysmaailman yhteen. Näet uusimmat jutut ja tutustu ammattilaisten maailmaan."
-                icon={<BiLink />}
-                />
-              </Col>
-              <Col>
-                <InfoCard 
-                number="04"
-                title="Hyväntuulinen tekemisen meininki"
-                text=" Meillä voi vapaasti kysellä ja ihmetellä, pääkohderyhmänä ovat opiskelijat."
-                icon={<AiFillSmile/>}
-                />
-              </Col>
-            </Row>
-          </Container>
+            <Container>
+              <Row xs={1} md={2} className="g-4">
+                <Col>
+                  <InfoCard
+                    number="01"
+                    title="MediaDay 2022"
+                    text="Metropolia ammattikorkeakoulu järjestää Mediaday-tapahtuman pitkän tauon jälkeen keväällä 2022."
+                    icon={<BiMoviePlay />}
+                  />
+                </Col>
+                <Col>
+                  <InfoCard
+                    number="02"
+                    title="Puhujia eri teollisuudenaloilta"
+                    text="Huipputason ammattilaisten puheenvuoroja, mukana onta rautaista puhujaa monelta mediakentän osa-alueelta."
+                    icon={<ImBubbles3 />}
+                  />
+                </Col>
+                <Col>
+                  <InfoCard
+                    number="03"
+                    title="Mediaday yhdistää"
+                    text="Mediaday tuo opiskelijat ja yritysmaailman yhteen. Näet uusimmat jutut ja tutustu ammattilaisten maailmaan."
+                    icon={<BiLink />}
+                  />
+                </Col>
+                <Col>
+                  <InfoCard
+                    number="04"
+                    title="Hyväntuulinen tekemisen meininki"
+                    text=" Meillä voi vapaasti kysellä ja ihmetellä, pääkohderyhmänä ovat opiskelijat."
+                    icon={<AiFillSmile />}
+                  />
+                </Col>
+              </Row>
+            </Container>
           </body>
         </Route>
       </Switch>
