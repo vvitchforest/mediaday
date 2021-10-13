ec925a504da9a3a95249717c843f6cde4d85d8d0
AiliAi <rebane@gmail.com>
2021-10-13 23:31:40 +0300

    calendar heading in mobile view

--- src/pages/SchedulePage.js
+++ src/pages/SchedulePage.js
@@ -7,10 +7,10 @@ import "../components/Calendar/calendar.scss";
 const SchedulePage = () => {
   return (
     <Container className="massive-margin text-center mb-5">
-      <h1 className="d-none d-sm-block main-heading-style">
+      <h1 className="main-heading-style">
         MediaDay aikataulu
       </h1>
-      <h2 className="d-none d-sm-block subheading-style">
+      <h2 className="subheading-style mb-4">
         23.03.2022, Karaportti 2
       </h2>
       <Row className="justify-content-md-center my-calendar-row">
